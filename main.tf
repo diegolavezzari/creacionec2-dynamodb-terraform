b0e11529a555a038e7e3a01155a8f1f8b661a305
Diego <diego.lavezzari@gmail.com>
2024-11-27 13:59:21 -0300

    resource tabla dynamodb

--- main.tf
+++ main.tf
@@ -32,3 +32,14 @@ resource "aws_dynamodb_table" "terraform_state_lock" {
     type = "S"
   }
 }
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-state-lock"  # Nombre de la tabla
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
